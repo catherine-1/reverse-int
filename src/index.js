ffcbab26d4cd6cac56e78853b59bf6a0e67c46d0
Katsiaryna Valachko <volochko.ekaterina@gmail.com>
2024-09-02 13:51:38 +0200

    feat: add entire function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+
+    n = Math.abs(n);
+
+    while (n % 10 === 0 && n !== 0) {
+        n = n / 10;
+    };
+
+    const nArray = n.toString().split('');
+    const reversedArray = nArray.reverse();
+    const reversedString = reversedArray.join('').toString();
+
+    return Number(reversedString);
 }
